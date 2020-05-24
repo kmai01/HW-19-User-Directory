2dd1b0799c195144b8a6a826bb7f379be7eb09b8
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-05-24 13:45:09 -0500

    add container

---
+++ client/src/components/SearchContainer.js
@@ -0,0 +1,14 @@
+import React ,{Component} from "react";
+import SearchForm from "./Search";
+import API from "../utils/API";
+
+class SearchContainer extends Component{
+
+    state={
+    
+    search:"",
+    result: [],
+
+    }
+}
+
